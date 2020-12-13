a73f1ac3151304a1702c0211ead7e1386ef3d34d
CHEOL HUN LEE <doulos2008@naver.cpm>
2020-12-13 23:23:01 +0100

    firebase setup

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MESUREMENT_ID,
+};
+
+const app = firebase.initializeApp(firebaseConfig);
+
+export default app;
