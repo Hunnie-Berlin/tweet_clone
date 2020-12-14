a733b826f05c9b888fd541c87082f3c0e4baa98a
CHEOL HUN LEE <doulos2008@naver.cpm>
2020-12-14 10:27:50 +0100

    Logout

--- src/components/Router.js
+++ src/components/Router.js
@@ -2,14 +2,30 @@ import React from "react";
 import { HashRouter, Route, Switch } from "react-router-dom";
 import Auth from "routes/Auth";
 import Home from "routes/Home";
+import Profile from "routes/Profile";
+import Navigation from "components/Navigation";
 
 const Router = ({ isLoggedIn }) => {
   return (
     <HashRouter>
+      {isLoggedIn && <Navigation />}
       <Switch>
-        <Route exact path="/">
-          {isLoggedIn ? <Home /> : <Auth />}
-        </Route>
+        {isLoggedIn ? (
+          <>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route exact path="/profile">
+              <Profile />
+            </Route>
+          </>
+        ) : (
+          <>
+            <Route exact path="/">
+              <Auth />
+            </Route>
+          </>
+        )}
       </Switch>
     </HashRouter>
   );
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { authService } from "myFirebase";
+import { useHistory } from "react-router-dom";
 
 const Profile = () => {
-  return <div>Profile</div>;
+  const history = useHistory();
+  const onClickLogOut = () => {
+    authService.signOut();
+    history.push("/");
+  };
+  return (
+    <>
+      <button onClick={onClickLogOut}>LOGOUT</button>
+    </>
+  );
 };
 
 export default Profile;
